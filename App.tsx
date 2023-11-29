f8688d1cca5bd471fa29b2948b1e3203581bbc99
MarioGoDevLike <marionassar57@gmail.com>
2023-11-30 00:21:45 +0200

    deleted old files

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import AppNavigation from './navigation/appNavigation';
+
+
+export default function App(){
+  return (
+    <AppNavigation />
+  );
+}
